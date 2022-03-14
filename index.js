83ee06077d426d5334c08273624cf214cf24c55b
haqqibrahim <enessyibrahim@gmail.com>
2022-03-14 09:48:02 +0100

    3 files modified, 1 added -m "3/14/2022 9:47AM"

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+// Set Public Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Body Parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// cors
+app.use(cors());
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('index');
 });
 
 app.listen(port, () => {
